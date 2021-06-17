d14a18aaed25afba9d2ac5c745447911f22407b2
dmnitprof <54173914+dmnitprof@users.noreply.github.com>
2021-06-18 00:09:40 +0300

    Edit size circle
    Edit size circle

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ function finishGame() {
 
 function createRandomCircle() {
     const circle = document.createElement('div')
-    const size = getRandomNumber(15, 65)
+    const size = getRandomNumber(25, 75)
     const {width, height} = board.getBoundingClientRect()
     const x = getRandomNumber(0, width - size)
     const y = getRandomNumber(0, height - size)
